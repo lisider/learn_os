219d101dacf2c47266d6e3a71046865efb22bd4f
lisider <1368317399@qq.com>
2019-06-25 07:19:53 -0700

    [boot_mmu][添加代码]

---
+++ boot_mmu/boot.c
@@ -0,0 +1,33 @@
+typedef void (*init_func)(void);
+
+#define UFCON0	((volatile unsigned int *)(0x50000020))
+
+void helloworld(void){
+	const char *p="helloworld\n";
+	while(*p){
+		*UFCON0=*p++;
+	};
+}
+
+void test_mmu(void){
+	const char *p="test_mmu\n";
+	while(*p){
+		*(volatile unsigned int *)0xd0000020=*p++;
+	};
+}
+
+static init_func init[]={
+	helloworld,
+	0,
+};
+
+void plat_boot(void){
+	int i;
+	for(i=0;init[i];i++){
+		init[i]();
+	}
+	init_sys_mmu();
+	start_mmu();
+	test_mmu();
+	while(1);
+}
